a4441bd6819eb77c8c612eaaaff4f1bc18b6781a
anne <messnerbeatrix@gmail.com>
2022-03-27 23:06:21 +0300

    etapa4

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const fs = require("fs");
 const sharp = require("sharp");
-// const (Client)=require("PG");
+const {Client} = require("pg");
+const ejs = require("ejs");
+const sass = require("sass");
 
 app = express();
 
-// client = new Client((user:"anne", password:"2017", database:"db_test", host:"localhost", port: 5432));
+var client = new Client({user: "anne", password: "2017", database: "bd_arason", host: "localhost", port: 5432});
+client.connect();
+
 
 
 
@@ -13,11 +17,16 @@ console.log(__dirname)
 
 app.set('view engine', 'ejs');
 app.use('/static', express.static(__dirname+'/static'))
+app.use('/temp', express.static(__dirname+'/temp'))
 
 
 app.get(["/", "/home", "/index"] , function(req,res)  {
-    res.render('pagini/index', {ip: req.ip, imagini:obImagini.imagini});
-    res.end()
+    client.query("select * from tabel", function(err, rezQuery){
+    console.log(rezQuery);
+    res.render('pagini/index', {ip: req.ip, imagini:obImagini.imagini, produse:rezQuery.rows});
+
+    })
+    // res.end()
 });
 
 app.get("/contactus", function(req,res)  {
@@ -25,20 +34,11 @@ app.get("/contactus", function(req,res)  {
     res.end()
 });
 
-app.get('/*.ejs', function(req, res){
-    res.status(403);
-    res.render('pagini/forbidden');
-});
 
-app.get('*', function(req, res){
-        res.status(404);
-        res.render('pagini/error404');
-});
-
-// ----------- curs 4 ------------
+// ----------- curs 4 / Galerie-statica ------------
 
 function creeazaImagini(){
-    var buf=fs.readFileSync(__dirname+"/static/json/galerie.json").toString("utf8");
+    var buf = fs.readFileSync(__dirname + "/static/json/galerie.json").toString("utf8");
     console.log(buf);
     obImagini = JSON.parse(buf); //global
 
@@ -68,6 +68,79 @@ function creeazaImagini(){
 creeazaImagini();
 
 //------------- end --------------
+
+
+
+//-------------Curs 5 / Galerie-animata -------------
+
+app.get("/galerie-animata", function(req, res) {
+    var sirScss = fs.readFileSync(__dirname + "/static/scss/galerie_animata.scss").toString("utf8");
+    var culori =["navy", "black", "purple", "gray"];
+    var indiceAleator = Math.floor(Math.random()*culori.length);
+    var culoareAleatoare= culori[indiceAleator];
+    rezScss = ejs.render(sirScss, {culoare: culoareAleatoare});
+    console.log(rezScss);
+    var caleScss = __dirname + "/temp/galerie-animata.scss";
+    fs.writeFileSync(caleScss, rezScss);
+
+    try {
+        rezCompilare = sass.compile(caleScss, {sourceMap: true});
+
+        var caleCss = __dirname + "/temp/galerie-animata.css";
+        fs.writeFileSync(caleCss, rezCompilare.css);
+        res.setHeader("Content-Type", "text/css");
+        res.sendFile(caleCss);
+    }
+   catch(err) {
+       console.log(err);
+       res.send("Eroare");
+   }
+
+});
+//------------- end ---------------------------
+
+// ------------ Error / Lab 5 --------------------
+
+function creeazaErori() {
+    var buf = fs.readFileSync(__dirname + "/static/json/erori.json").toString("utf8");
+    obErori=JSON.parse(buf); //global
+
+    console.log(obErori);
+}
+creeazaErori();
+
+// randeazaEroare(404,true)
+function randeazaErori(res, identificator, status, titlu, text, imagine){
+    var eroare = obErori.erori.find(function(elem){ return elem.identificator == identificator});
+    if(status){
+        res.status(identificator).render("pagini/eroare_generala", {titlu: eroare.titlu, text: eroare.text, imagine: obErori.cale_baza + "/" + eroare.imagine});
+    } 
+    else{ 
+         let titlu = titlu || eroare.titlu;
+         let text = text || eroare.text;
+         let imagine = imagine || obErori.cale_baza + "/" + eroare.imagine;
+
+        res.render("pagini/eroare_generala", {titlu: titlu, text: text, imagine: imagine});
+    }
+}
+// -------------- end ------------------------------
+
+//------------- Error 404/ Error 403 -----------------
+
+app.get('/*.ejs', function(req, res){
+    randeazaErori(res,403,true)
+
+    // res.status(403);
+    // res.render('pagini/forbidden');
+});
+
+app.get('*', function(req, res){
+        randeazaErori(res,404,true)
+        // res.status(404);
+        // res.render('pagini/error404');
+});
+//------------- end --------------
+
 
 app.listen(8080);
 console.log("A pornit!");
